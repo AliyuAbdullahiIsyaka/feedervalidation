b8868fc710d16132ea51babd3df67c02c5eeee47
Aliyu Abdullahi <88856370+AliyuAbdullahiIsyaka@users.noreply.github.com>
2025-11-20 10:01:38 +0100

    Add context menu prevention on right-click
    Prevent default context menu on right-click in the app.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import "../styles/TableStyles.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleContextMenu = (e) => e.preventDefault();
+      window.addEventListener("contextmenu", handleContextMenu);
+
+      return () => {
+        window.removeEventListener("contextmenu", handleContextMenu);
+      };
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
